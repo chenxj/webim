187a75ec4ce0cf4426ec10cebef55406f59db32c
chenxj <chen1986jay@gmail.com>
2010-01-22 19:47:22 +0800

    fix sql

--- data/webim.sql
+++ data/webim.sql
@@ -2,29 +2,31 @@
 -- webim database schema
 --
 
-create TABLE webim_histories (
-	`uid` int(11) unsigned NOT NULL auto_increment,
-	`send` tinyint (1),
-	`type` varchar(20),
-	`to` mediumint(8) unsigned not null default '0',
-	`from` mediumint(8) unsigned not null default '0',
-	`body` text,
-	`style` varchar(150),
-	`timestamp` double,
-	`todel` TINYINT( 1 ) NOT NULL DEFAULT '0',
-	`fromdel` TINYINT( 1 ) NOT NULL DEFAULT '0',
-	PRIMARY KEY (uid),
-	KEY `todel` (`todel`),
-	KEY `fromdel` (`fromdel`),
-	KEY `timestamp` (`timestamp`),
-	KEY `to` (`to`),
-	KEY `from` (`from`),
-	KEY `send` (`send`)
-) ENGINE=MyISAM; 
+CREATE TABLE  `webim_histories` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `send` tinyint(1) DEFAULT NULL,
+  `type` varchar(20) DEFAULT NULL,
+  `to` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `from` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `body` text,
+  `style` varchar(150) DEFAULT NULL,
+  `timestamp` double DEFAULT NULL,
+  `todel` tinyint(1) NOT NULL DEFAULT '0',
+  `fromdel` tinyint(1) NOT NULL DEFAULT '0',
+  `uid` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `todel` (`todel`),
+  KEY `fromdel` (`fromdel`),
+  KEY `timestamp` (`timestamp`),
+  KEY `to` (`to`),
+  KEY `from` (`from`),
+  KEY `send` (`send`)
+) ENGINE=MyISAM AUTO_INCREMENT=1000372 DEFAULT CHARSET=utf8;
 
-create TABLE webim_setting (
-	`uid` mediumint(8) unsigned not null default '0',
-	`web` blob,
-	`air` blob,
-	PRIMARY KEY (uid)
-) ENGINE=MyISAM;
+CREATE TABLE  `webim_setting` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `web` blob,
+  `air` blob,
+  `uid` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;

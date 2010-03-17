157d6c7e3639352ea3d20fb1d71cf40870216530
freefis <freefis@gmail.com>
2010-03-17 08:58:19 +0800

    install charset

--- unify/install.php
+++ unify/install.php
@@ -535,7 +535,7 @@ function show_header() {
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=$_SC[charset]" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title> UCIM2.0透明幻想(Transparent Fantasy)版本程序安装 </title>
 	<style type="text/css">
 	* {font-size:12px; font-family: Verdana, Arial, Helvetica, sans-serif; line-height: 1.5em; word-break: break-all; }

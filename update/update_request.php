ca00b72d9010263a0d62f97aa396b5b38a8d11c3
chenxj <chen1986jay@gmail.com>
2010-04-03 23:10:25 +0800

    update...

--- update/update_request.php
+++ update/update_request.php
@@ -1,26 +1,27 @@
 ﻿<?php
+	include_once('common.php');
 	global $version_info;
-	if(gp['cmd'] === 'ClearState'){
+	if(gp('cmd') === 'ClearState'){
 		if(!clearState()){
 			echo "false";
 		}
 		echo "true";
-	}else if(gp['cmd'] === 'GetNewestVersionInfo'){
+	}else if(gp('cmd') === 'GetNewestVersionInfo'){
 		$version_info = getNewestVersionInfo();
 		if(!$version_info){
 			echo getCurrentState();
 		}else{
 			echo getCurrentState();
 		}
-	}else if(gp['cmd'] === 'Update'){
+	}else if(gp('cmd') === 'Update'){
 		if(!update($version)){
 			echo getCurrentState();
 		}else{
 			echo getCurrentState();
 		}
-	}else if(gp['cmd'] === 'Rollback'){
+	}else if(gp('cmd') === 'Rollback'){
 		roll_back();
-	}else if(gp['cmd'] === 'GetCurrentState'){
+	}else if(gp('cmd') === 'GetCurrentState'){
 		echo getCurrentState();
 	}
 ?>

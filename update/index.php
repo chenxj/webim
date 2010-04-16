b324cb7a8795e488e42bdfa8b1c6df6d4c855c46
chenxj <chen1986jay@gmail.com>
2010-04-16 21:13:25 +0800

    u

--- update/index.php
+++ update/index.php
@@ -15,7 +15,10 @@ function preProcess(data){
 }
 var iscontinue = true;
 var pollable = true;
+var percent = 10;
+var step = 1;
 function versionUpdate(){
+ 
 	pollable = true;
 	$("#update_ctl").attr('disabled',true);
 	$.ajax({url:'update_request.php',
@@ -26,13 +29,14 @@ function versionUpdate(){
 			}catch(e){
 					
 			}
-			poll("RollBack");
+			
 		},
 		error:function(req,status,err){
 				$("#update_ctl").attr('disabled',false);
 				pollable = false;
 		}
 	});
+	poll("Update");
 }
 function rollBack(){
 	pollable = true;
@@ -46,14 +50,14 @@ function rollBack(){
 					
 				}
 				
-				poll("RollBack");
+				
 			},
 			error:function(req,status,err){
 				$("#rollback_ctl").attr('disabled',false);
 				pollable = false;
 			}
 	});
-
+	poll("RollBack");
 }
 function progressing(){
 	var dom = $("#progress_simbol");
@@ -68,8 +72,8 @@ function progressing(){
 function showProgress(msg,percent){
 	$("#status").css("visibility","visible");
 	$("#progress_txt").html(msg);
-	$("#spaceuesd1").progressBar(percent);
-	$("#update_ctl").attr('disabled',true);
+/*	 $("#spaceused1").progressBar(percent);
+	$("#update_ctl").attr('disabled',true);*/
 }
 function checkbtnusable(){
 		$.ajax({
@@ -92,79 +96,96 @@ function checkbtnusable(){
 				
 			}});
 }
+
+function init(){
+		$("#spaceused1").progressBar({height:12,width:120,	barImage:'images/progressbg_green.gif'});
+ 		$("#errmsg").css("display","none");
+ 		$("#update_ctl").attr('disabled',true);
+		$("#rollback_ctl").attr('disabled',true);
+}
+
+function control(){
+		$("#update_ctl").attr('disabled',false);
+		$("#rollback_ctl").attr('disabled',false);	
+}
 function poll(preAction){
-	
+	 
 	var Action = "";
+  
 	$.ajax({
-			url:"update_request.php",
-			data:{"cmd":"GetCurrentState"},
+			url:"status.php",
 			success:function(data){
 				var iscontinue = true;
-				
+				if (data == ""){
+						control();
+						return;
+				}
 				try{
 					data = jQuery.parseJSON(data);
 				}catch(e){
 					//alert(e);	
 					iscontinue=false;
+					return;
 				}
 				
 				Action = data.state;
- 
-				if (data.isok){
+ 				if (data.percent && data.percent == 100){
+ 					percent = 100;	
+ 				}else{
+ 					percent += (step/20);	
+ 				}
+				if (!data.isok){
 					//finished
 					$("#status").css("visibility","hidden");
 					return ;
 				}
 				switch (data.state){
 					case "Rollback":
-						showProgress("回滚现有版本...",data.percent);
+						showProgress("回滚现有版本...",percent);
 						break;
 					//downloading...
 					case "Download":
-						showProgress("下载中...",data.percent);
+						showProgress("下载中...",percent);
 						//disable update button
 						break;
 					//updating
 					case "Update":
-						showProgress("更新中...",data.percent);
+						showProgress("更新中...",percent);
 						break;
 					case "Backup":
-						showProgress("备份现有版本...",data.percent);
+						showProgress("备份现有版本...",percent);
 						break;
 					}
-				}
-			});
-			iscontinue && pollable && setTimeout(function(){poll(Action);},2500);;
-		}
+				},
+			error:function(req,txt,err){
+				iscontinue = false;		
+			}
+		});
+			iscontinue && pollable && setTimeout(function(){poll(Action); },2500);
+}
 		
  
 $(document).ready(function() {
 	//init progressbar 
-	$("#spaceused1").progressBar({height:12,width:120,	barImage:'images/progressbg_green.gif'});
-				// request to get newest version
-	//getVersion();
-	//poll("");
 		$.ajax({
-			url:"update_request.php",
-			data:{"cmd":"GetNewestVersionInfo"},
+			url:"check.php",
 			success:function(data){
 				try{
 					data = jQuery.parseJSON(data);
 				}catch(e){
-					//err of return data;
-					//alert(e);	
-					
+					$("#errmsg").css("display","");
 				}
+				
 				//no update, 
-				if (!data.Version){
+				/*if (!data.Version){
 					$("#version_txt").html("当前为最新版本");
 					$("#update_ctl").attr('disabled',true);
 					return ;
+				}*/
+				if (data.updata_now == 1){
+					$("#version_txt").html("可更新版本 "+data.version );
 				}
-				$("#version_txt").html("V"+data.Version+"版本新特性");
-				$("#update_ctl").attr('disabled',false);
-				$("#rollback_ctl").attr('disabled',false);
-				//poll("");
+				poll("");
 			},
 			error:function(req,txt,err){
 			},
@@ -189,13 +210,14 @@ $(document).ready(function() {
 					<li>采用与Facebook一样的标准HTML界面设计</li>
 					<li>集群服务器1,000,000并发用户支持</li>
 				</ul>
+			</div>
 			<div id="errmsg">
-				<font color="red">出错啦,请尝试刷新页面，或者手动升级/回滚</font>	
+				<font color="red">出错啦,请尝试刷新页面</font>	
 			</div>
-			</div>
+			
 			<div id="status">
 				<div id="progress"><span id="progress_txt"></span><span id="progress_simbol"></span></div>
-				<span class="progressBar" id="spaceused1"></span>
+				<span class="progressBar" id="spaceused12"></span>
 			</div>
 		</div>
 			<div id="control">

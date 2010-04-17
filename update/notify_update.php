0b5b683b80ceb0d7fcfc5b19ee65f38e200d20ac
freefis <freefis@gmail.com>
2010-04-18 00:19:44 +0800

    增加管理员群

--- update/notify_update.php
+++ update/notify_update.php
@@ -11,29 +11,28 @@ $platform = which_platform();
 switch($platform){
     case 'uchome':
         include_once($_IMC["install_path"] .'webim/lib/uchome.php');
+        $db_obj = $_SGLOBAL['db'];
         break;
     case 'discuz':
         include_once($_IMC["install_path"] .'webim/lib/discuz.php');
+        $db_obj = $db;
         break;
 }
+
+$admins = explode(",",$_IMC['admin_ids']);
 
 if ($latest_version == $cur_version){
     //do nothing
 }
 else{
-    $time = microtime(true)*1000;
-	$body = from_utf8("WebIM有新的更新！请访问".$_IMC['install_url']."update/index.php了解详情!");
-	$columns = "`uid`,`send`,`to`,`from`,`style`,`body`,`timestamp`,`type`";
-	$values_from = "'1','0','1','webim','','$body','$time','unicast'";
-	
-	if ($platform === 'uchome')
-	{
-		$_SGLOBAL['db']->query("INSERT INTO ".im_tname('histories')." ($columns) VALUES ($values_from)");
-	}
-	else if ($platform === 'discuz')
-	{
-		$db->query("INSERT INTO ".im_tname('histories')." ($columns) VALUES ($values_from)");
-	}
+    foreach($admins as $admin)
+    {
+        $time = microtime(true)*1000;
+        $body = from_utf8("WebIM有新的更新！请访问".$_IMC['install_url']."update/index.php了解详情!");
+        $columns = "`uid`,`send`,`to`,`from`,`style`,`body`,`timestamp`,`type`";
+        $values_from = "'1','0','$admin','webim','','$body','$time','unicast'";
+        $db_obj->query("INSERT INTO ".im_tname('histories')." ($columns) VALUES ($values_from)");
+    }
 }
 
 function which_platform(){

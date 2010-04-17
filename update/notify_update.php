706314d5c053e1cb413a1850c1af2e8dd41799a9
xue <xue@aspiretech.(none)>
2010-04-17 22:05:07 +0800

    update notify_update.php repair the db bug

--- update/notify_update.php
+++ update/notify_update.php
@@ -5,6 +5,18 @@ $latest_version = explode("\n",file_get_contents($url));
 $latest_version = $latest_version[0];
 $cur_version = $_IMC['version'];
 $data = array("version"=>$latest_version);
+
+$platform = which_platform();
+
+switch($platform){
+    case 'uchome':
+        include_once($_IMC["install_url"] .'webim/lib/uchome.php');
+        break;
+    case 'discuz':
+        include_once($_IMC["install_url"] .'webim/lib/discuz.php');
+        break;
+}
+
 if ($latest_version == $cur_version){
     //do nothing
 }
@@ -13,6 +25,29 @@ else{
 	$body = from_utf8("WebIM有新的更新！请访问".$_IMC['install_url']."update/index.php了解详情!");
 	$columns = "`uid`,`send`,`to`,`from`,`style`,`body`,`timestamp`,`type`";
 	$values_from = "'1','0','1','webim','','$body','$time','unicast'";
-	$_SGLOBAL['db']->query("INSERT INTO ".im_tname('histories')." ($columns) VALUES ($values_from)");
+	
+	if ($platform === 'uchome')
+	{
+		$_SGLOBAL['db']->query("INSERT INTO ".im_tname('histories')." ($columns) VALUES ($values_from)");
+	}
+	else if ($platform === 'discuz')
+	{
+		$db->query("INSERT INTO ".im_tname('histories')." ($columns) VALUES ($values_from)");
+	}
 }
+
+function which_platform(){
+	/*
+	 *  check the platform 
+	 *  Uchome ? Discuz ?  PhpWind?
+	 *
+	 */
+	if(file_exists($_IMC["install_path"].'data')){
+		return "uchome";
+	}
+	if(file_exists($_IMC["install_path"].'forumdata')){
+		return "discuz";
+	}
+}
+
 ?>

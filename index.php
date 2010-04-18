341cf5d2ab8d370044cdac533732c850cb0fb92c
freefis <freefis@gmail.com>
2010-04-18 11:19:52 +0800

    update utf8

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<html>
-
-
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
 <pre>
 <h2><a href="./update/">自动更新NextIM</a></h2>
 </pre>

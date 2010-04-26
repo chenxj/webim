f9dfb2591c7a2b3f6b1e7e0a11cf4003206172e6
imcom <jinyu.bjut@gmail.com>
2010-04-26 22:03:53 +0800

    fix hotpost

--- api/hotpost.php
+++ api/hotpost.php
@@ -1,36 +1,8 @@
 <?php 
-
+$configRoot = '..' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR ;
+include_once($configRoot . 'discuz.php');
 include_once('discuz.php');
-/*//DISUCZ获取新贴SQL
-
-$query = $db->query("SELECT t.*, f.name FROM {$tablepre}threads t, {$tablepre}forums f WHERE t.fid<>'$fid' AND f.fid=t.fid AND f.fid not in (0) AND t.displayorder not in (-1,-2) ORDER BY t.dateline DESC LIMIT 0, 10");
-
-//DISUCZ获取新回复SQL
-
-$query = $db->query("SELECT t.*, f.name FROM {$tablepre}threads t, {$tablepre}forums f WHERE f.fid=t.fid AND t.closed NOT LIKE 'moved|%' AND t.replies !=0 AND f.fid not in (0) AND t.displayorder not in (-1,-2) ORDER BY t.lastpost DESC LIMIT 0, 10");
-
-//DISUCZ获取热帖SQL
-
-$new_hot_threadlist = array();
-
-$mthread = array();
-
-$ctime=$timestamp-3600*24*7;//最后7是天数为本周 
-
-$query = $db->query("SELECT t.*, f.name FROM {$tablepre}threads t, {$tablepre}forums f WHERE t.fid<>'$fid' AND f.fid=t.fid AND t.closed NOT LIKE 'moved|%' AND t.replies !=0 AND t.dateline>$ctime AND f.fid not in (0) AND t.displayorder not in (-1,-2) ORDER BY t.replies DESC LIMIT 0, 10");
-
-//DISUCZ获取今日发贴排行SQL
-
-$tomonth=date(n);
-
-$todate=date(j);
-
-$toyear=date(Y);
-
-$time=mktime(0,0,0,$tomonth,$todate,$toyear);
-
-$query=$db->query("select count(pid) as num,authorid,author from $tablepre"."posts where dateline>=$time group by authorid order by num desc limit 0,10");
-		*/	
+	
 	$today = $timestamp - ($timestamp + $timeoffset * 3600) % 86400;
 	
 	$ctime=$timestamp-3600*24*7;

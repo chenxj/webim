b20e54a6b9e35f73efac5e7bab9859057c9c8d8a
free.wang <freefis@localhost.localdomain>
2010-04-28 19:54:26 +0800

    instal bug

--- install/index.php
+++ install/index.php
@@ -727,7 +727,7 @@ function insertconfig($s, $find, $replace) {
 
 function write_webim_config($file,$domain,$apikey,$theme,$charset,$broadcastid=null) {
 	global $url_path, $file_path, $platform,$nextim_version;
-	foreach($file_path as &$var){
+	foreach($file_path as $var){
 		$var = str_replace('\\', '/', $var);
 		$var = str_replace('//', '/', $var);
 	}

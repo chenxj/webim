157109505eafcf4b1018af64d00251e9b7bc205e
Jinyu <jinyu@jinyu-laptop.(none)>
2010-05-08 22:09:53 +0800

    new pw user_info

--- api/user_info.php
+++ api/user_info.php
@@ -8,35 +8,13 @@ switch($platform){
 	case 'uchome':
 		include_once($configRoot . 'uchome.php');
 		break;
+	case 'phpwind':
+		include_once($configRoot . 'phpwind.php');
 }
-if($platform === 'uchome'){
 	$ids = gp('ids');
 	if(empty($ids)){
         	echo "[]";
         	exit();
 	}
 	echo json_encode(find_buddy($ids));
-        //$output['buddies'] = find_buddy($buddy_ids);
-}else if($platform === 'discuz'){
-/*
-	session_start();
-	$friends = $_SESSION['friend_ids'];
-	$strangers = $_SESSION['stranger_ids'];
-	$buddy_online_ids = $_SESSION['online_ids'];
-        foreach($friend_ids as $id){
-                if(in_array($id, $buddy_online_ids)){
-                        $friends[] = $id;
-                }
-                $strangers = ids_except($id, $buddy_online_ids);
-        }
-        echo json_encode(find_buddy($strangers, $friends));
-*/
-$ids = gp('ids');
-        if(empty($ids)){
-                echo "[]";
-                exit();
-        }
-        echo json_encode(find_buddy($ids));
-
-}
 ?>

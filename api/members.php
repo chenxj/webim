cd630faa3794f8cb5ba06ec15240417b16018d4c
freefis <freefis@gmail.com>
2010-05-13 05:04:59 +0800

    updatemembes

--- api/members.php
+++ api/members.php
@@ -3,18 +3,10 @@ $platform = $_GET['platform'];
 $configRoot = '..' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR ;
 include_once( $configRoot . 'http_client.php');
 
-switch($platform){
-	case 'discuz':
-		include_once($configRoot . 'discuz.php');
-		break;
-	case 'uchome':
-		include_once($configRoot . 'uchome.php');
-		break;
-	case 'phpwind':
-		include_once($configRoot . 'phpwind.php');
-		break;
-}
-$platform = $_GET['platform'];
+include_once($configRoot . "{$platform}.php");
+
+echo 23;
+
 $ticket = gp('ticket');
 $room_id = gp('id');
 if(empty($ticket)) {

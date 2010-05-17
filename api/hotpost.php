96f09df413dcb9be1a02d236bf0473c3f8674b72
freefis <freefis@gmail.com>
2010-05-17 21:21:54 +0800

    update ways

--- api/hotpost.php
+++ api/hotpost.php
@@ -1,12 +1,13 @@
 <?php 
 $configRoot = '..' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR ;
 include_once($configRoot . 'discuz.php');
+$discuz_uid = $cuid;
 	
 	$today = $timestamp - ($timestamp + $timeoffset * 3600) % 86400;
 	
 	$ctime=$timestamp-3600*24*7;
 	$sql="SELECT t.* FROM {$tablepre}threads t, {$tablepre}forumfields f, {$tablepre}members m WHERE m.uid='$discuz_uid' and (INSTR(f.viewperm,m.groupid)>0 OR f.viewperm='') AND t.replies !=0  and t.fid<>'$fid' AND f.fid=t.fid AND t.closed NOT LIKE 'moved|%'  AND t.dateline>$ctime AND f.fid not in (0) AND t.displayorder not in (-1,-2) ORDER BY t.replies DESC LIMIT 0, 10";
-	$_SGLOBAL['db']->query("set NAMES ". UC_DBCHARSET);
+	$_SGLOBAL['db']->query("set NAMES ". "gbk");
 	$query = $_SGLOBAL['db']->query($sql);
 	while ($value = $_SGLOBAL['db']->fetch_array($query)) {
 $pm['daterange'] = 5;
@@ -19,7 +20,7 @@ $pm['daterange'] = 5;
 		}
 		$value['date'] = gmdate($dateformat, $value['lastpost'] + $timeoffset * 3600);
 		$value['time'] = gmdate($timeformat, $value['lastpost'] + $timeoffset * 3600);
-				$pmlist[]= array('from'=>$value['author'],'text'=>$value['subject'].'<span style=color:red>('.$value['replies'].')</span>','link'=>'viewthread.php?tid='.$value['tid'],'time'=>$value['time']);
+				$pmlist[]= array('from'=>$value['author'],'text'=>to_utf8($value['subject']).'<span style=color:red>('.$value['replies'].')</span>','link'=>'viewthread.php?tid='.$value['tid'],'time'=>$value['time']);
 			}
 
 	exit(json_encode($pmlist));

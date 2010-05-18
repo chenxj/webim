b7bc28bb5c9c40a930e058232d171ee5af8d3643
freefis <freefis@gmail.com>
2010-05-18 11:26:27 +0800

    del update  module

--- update/download_list.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once('common.php');
-if(getDownloadList()){
-	echo json_encode(array("state"=>"DownloadList", "isok"=>true, "iswait"=>false, "errmsg"=>"", "percent"=>""));
-}else{
-	echo json_encode(array("state"=>"DownloadList", "isok"=>false, "iswait"=>false, "errmsg"=>"Get download_list failed", "percent"=>""));
-}
-?>
--- update/getversion.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once('../config.php');
-echo "{\"version\":\"".$_IMC['version']."\"}";
-?>

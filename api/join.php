2abdfff954ab47c726d26695c390c1a65bd520e3
freefis <freefis@gmail.com>
2010-06-21 16:17:52 +0800

    up

--- api/join.php
+++ api/join.php
@@ -2,21 +2,16 @@
 $platform = $_GET['platform'];
 $configRoot = '..' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR ;
 include_once($configRoot . 'http_client.php');
+include_once($configRoot . 'common.php');
 
-switch($platform){
-	case 'discuz':
-		include_once($configRoot . 'discuz.php');
-		break;
-	case 'uchome':
-		include_once($configRoot . 'uchome.php');
-		break;
-}
+
 
 $ticket = gp('ticket');
 $room_id = gp('id');
 $nick = gp('nick');
 //TODO: should get nick from database
 if(!empty($ticket)) {
+    $space = my_info();
     $data = array('ticket'=>$ticket, 'domain'=>$_IMC['domain'],'nick'=>$nick, 'apikey'=>$_IMC['apikey'], 'room'=>$room_id, 'endpoint' => $space['uid']);
 	$client = new HttpClient($_IMC['imsvr'], $_IMC['impost']);
 	$client->post('/room/join', $data);
--- api/leave.php
+++ api/leave.php
@@ -2,19 +2,13 @@
 $platform = $_GET['platform'];
 $configRoot = '..' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR ;
 include_once($configRoot . 'http_client.php');
+include_once($configRoot . 'common.php');
 
-switch($platform){
-	case 'discuz':
-		include_once($configRoot . 'discuz.php');
-		break;
-	case 'uchome':
-		include_once($configRoot . 'uchome.php');
-		break;
-}
 $ticket = gp('ticket');
 $room_id = gp('id');
 $nick = gp('nick');
 if(!empty($ticket)) {
+   $space = my_info();
   $data = array('ticket'=>$ticket,'nick'=>$nick, 'domain'=>$_IMC['domain'], 'apikey'=>$_IMC['apikey'], 'room'=>$room_id, 'endpoint' => $space['uid']);
 	$client = new HttpClient($_IMC['imsvr'], $_IMC['impost']);
 	$client->post('/room/leave', $data);

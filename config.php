0c939f77437d3b42d3ef2004e02b5b301c544c48
chenxj <chen1986jay@gmail.com>
2010-06-29 01:48:33 +0800

    config

---
+++ config.php
@@ -0,0 +1,51 @@
+<?php
+$_IMC = array();
+$_IMC["enable"] = true;
+$_IMC["domain"] = "localhost";
+$_IMC["apikey"] = "public";
+$_IMC["imsvr"] = "192.168.66.128";
+$_IMC["impost"] = 9000;
+$_IMC["impoll"] = 8000;
+$_IMC["imsockecdp"] = 7050;
+$_IMC["theme"] = "flick";
+$_IMC["local"] = "zh-CN";
+$_IMC["charset"] = "zh-CN_gbk";
+$_IMC["buddy_name"] = "username";
+$_IMC["room_id_pre"] = 1000000;
+$_IMC["groupchat"] = true;
+$_IMC["emot"] = "default";
+$_IMC["opacity"] = 80;
+$_IMC["uchome_path"] = "";
+$_IMC["install_path"] = "/var/www/pw/u/";
+$_IMC["discuz_path"] = "/var/www/pw/u/webim/";
+$_IMC["uchome_url"] = "";
+$_IMC["discuz_url"] = "http://192.168.66.128/pw/u/webim/";
+$_IMC["version"] = "2.2.28";
+$_IMC["update"] = 0;
+$_IMC_LOG_TYPE["update_file"] = "UPDATE";
+$_IMC_LOG_TYPE["backup_project"] = "BACKUP";
+$_IMC_LOG_FILE["name"] = "./update.log";
+$_IMC_BACKUP["director"] = "../WEBIM_BAK";
+$_IMC["update_url"] = "http://update.nextim.cn/";
+$_IMC["admin_ids"] = "1,8,888,489839";
+$_IMC["timestamp"] = 10;
+$_IMC["platform"] = "discuz";
+$_IMC["crossdomain"] = true;
+
+/*
+ * =================================================================
+ * This line below for Engineer Configure Platform Info for NextIM
+ * =================================================================
+ */
+/* User Name in Platform Session  */
+$_IMC_PLF['username'] = $cusername;
+/* User ID in Platform Session  */
+$_IMC_PLF['uid']      = $cuid;
+
+
+
+
+
+
+
+
